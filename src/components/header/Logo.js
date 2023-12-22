274c022948746e11acdfa0bb70d493287c642d07
Dan Chui <dchui108@gmail.com>
2023-12-22 21:51:24 +0900

    Feat NavBar component

---
+++ src/components/header/Logo.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+import profileImg from '@/public/profile-img.png';
+import Image from 'next/image';
+
+const Logo = () => {
+  return (
+    <Link href="/" className="flex items-center text-dark">
+      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
+        <Image
+          src={profileImg}
+          alt="Dan Chui"
+          className="w-full h-auto rounded-full"
+        />
+      </div>
+      <span className="font-bold text-xl">Dan Chui</span>
+    </Link>
+  );
+};
+
+export default Logo;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import Logo from './Logo';
+import Link from 'next/link';
+import { CodeSlashIcon, GitHubIcon, LinkedInIcon, SunIcon } from '../Icons';
 
 const Header = () => {
-  return <div>index</div>;
+  return (
+    <header className="w-full p-4 px-10 flex items-center justify-between">
+      <Logo />
+      <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mr-2">
+          Contact
+        </Link>
+        <button>
+          <SunIcon />
+        </button>
+      </nav>
+      <div>
+        <a
+          href="https://www.danchui.dev/"
+          className="inline-block w-6 h-6 mr-4"
+        >
+          <CodeSlashIcon className="hover:scale-125 transition-all duration-200" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/danchui/"
+          className="inline-block w-6 h-6 mr-4"
+        >
+          <LinkedInIcon className="hover:scale-125 transition-all duration-200" />
+        </a>
+        <a
+          href="https://github.com/dan-chui"
+          className="inline-block w-6 h-6 mr-4"
+        >
+          <GitHubIcon className="hover:scale-125 transition-all duration-200" />
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
