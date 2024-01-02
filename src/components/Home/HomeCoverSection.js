fca46d16cb8e165f1cb605d8821a1d3f5a14891b
Dan Chui <dchui108@gmail.com>
2024-01-02 16:55:22 +0900

    Style Home Cover section for responsiveness

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -17,11 +17,6 @@ const HomeCoverSection = ({ blogs }) => {
             bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0
             "
         />
-        <div
-          className="absolute top-0 left-0 bottom-0 right-0 h-full
-        bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0
-        "
-        />
         <Image
           src={blog.image.filePath.replace('../public', '')}
           placeholder="blur"
@@ -29,14 +24,13 @@ const HomeCoverSection = ({ blogs }) => {
           alt={blog.title}
           fill
           className="w-full h-full object-center object-cover rounded-3xl -z-10"
+          sizes="100vw"
+          priority
         />
-        <div className="w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light">
+        <div className="w-full lg:w-3/4 p-6 sm:p-8 md:p-12  lg:p-16 flex flex-col items-start justify-center z-0 text-light">
+          <Tag link={`/categories/${slug(blog.tags[0])}`} name={blog.tags[0]} />
           <Link href={blog.url} className="mt-6">
-            <Tag
-              link={`/categories/${slug(blog.tags[0])}`}
-              name={blog.tags[0]}
-            />
-            <h1 className="font-bold capitalize text-4xl mt-3">
+            <h1 className="font-bold capitalize text-lg sm:text-xl md:text-3xl lg:text-4xl">
               <span
                 className="bg-gradient-to-r from-accent to-accent dark:from-accentDark/50 
                 dark:to-accentDark/50 bg-[length:0px_6px]
@@ -46,7 +40,7 @@ const HomeCoverSection = ({ blogs }) => {
               </span>
             </h1>
           </Link>
-          <p className="inline-block mt-4 text-xl font-in">
+          <p className="hidden  sm:inline-block mt-4 md:text-lg lg:text-xl font-in">
             {blog.description}
           </p>
         </div>
