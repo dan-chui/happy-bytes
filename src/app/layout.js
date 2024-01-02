573d490d4e16a5c04946e165bc2ebec0ad655080
Dan Chui <dchui108@gmail.com>
2024-01-02 11:22:51 +0900

    Feat add Light and Dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
-import { cx } from '@/src/utils';
 import './globals.css';
+import { cx } from '@/src/utils';
 import { Inter, Manrope } from 'next/font/google';
 import Header from '@/src/components/Header';
 import Footer from '../components/Footer';
 import siteMetadata from '../utils/siteMetaData';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -51,8 +52,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={cx(inter.variable, manrope.variable, 'font-mr bg-light')}
+        className={cx(
+          inter.variable,
+          manrope.variable,
+          'font-mr bg-light dark:bg-dark'
+        )}
       >
+        <Script id="theme-switcher" strategy="beforeInteractive">
+          {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }`}
+        </Script>
         <Header />
         {children}
         <Footer />
